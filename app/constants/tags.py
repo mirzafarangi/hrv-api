1859887a3be66c2cf5c85e4383e9a1b208d0bf29
Ashkan Beheshti <ashimashi@posteo.de>
2025-03-28 23:54:26 +0100

    tags fix

--- app/constants/tags.py
+++ app/constants/tags.py
@@ -1,17 +1,4 @@
 # File: constants/tags.py
 ACCEPTED_TAGS = [
-    "Dog_Walking",
-    "Work_Walking",
-    "Movie",
-    "brunch",
-    "evening meal",
-    "late meal",
-    "coding",
-    "meditation",
-    "Sleep",
-    "workout",
-    "Swimming",
-    "Creative",
-    "fatigue",
-    "anxiety"
+    "Sleep", "Dog_Walking", "Work_Walking", "Rest","Workout", "WorkLife", "Fatigue","Pich"
 ]
